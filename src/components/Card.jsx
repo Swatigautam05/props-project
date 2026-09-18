25eeded6fd32db33482977d55530df71971c6680
Swati Gautam <gautamswati510@gmail.com>
2026-09-18 15:29:03 +0530

    Test without external images

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ const Card = (props) => {
   return (
    <div className="card">
         <div className="top">
-          <img src={props.logo} alt=""></img>
+          <img src="/vite.svg" alt=""></img>
           <button>Save<Bookmark  size={12}  /></button>
         </div>
         <div className="center">
